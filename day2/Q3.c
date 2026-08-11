41fe4723f3265bb1abe09d4a34f5a6222562f925
Aditi Bansal <aditibansal7355@gmail.com>
2026-08-12 00:04:44 +0530

    commit

--- day2/Q3.c
+++ day2/Q3.c
@@ -1,15 +1,17 @@
 /*
-C program to calculate area and perimeter of rectangle
+Program to calculate the area and perimeter of a rectangle
 */
 #include <stdio.h>
-int main() 
-{
+
+int main() {
     int length, breadth;
     int area, perimeter;
+    // Read length and breadth from standard input
     scanf("%d %d", &length, &breadth);
+    // Perform rectangle calculations
     area = length * breadth;
     perimeter = 2 * (length + breadth);
-    // Display result
+    // Print output in required format: Area=X, Perimeter=Y
     printf("Area=%d, Perimeter=%d\n", area, perimeter);
     return 0;
 }
