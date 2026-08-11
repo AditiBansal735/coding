f4fe0f974f6f7f4b5f0934462d9a0d8862bee9e7
Aditi Bansal <aditibansal7355@gmail.com>
2026-08-11 23:21:17 +0530

    commit

--- day1/Q2.c
+++ day1/Q2.c
@@ -1,24 +1,21 @@
+/*
+a program to input two numbers and display their sum
+*/
+
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    int sum, diff, prod;
+    int a, b, c;
+
     printf("Enter the value of a: ");
     scanf("%d", &a);
+
     printf("Enter the value of b: ");
     scanf("%d", &b);
-    sum = a + b;
-    printf("The sum of %d and %d is %d\n", a, b, sum);
-    diff = a - b;
-    printf("The difference of %d and %d is %d\n", a, b, diff);
-    prod = a * b;
-    printf("The product of %d and %d is %d\n", a, b, prod);
-    /*Prevent division by zero and cast to float for decimal accuracy*/
-    if (b != 0) {
-        float quotient = (float)a / b;
-        printf("The quotient of %d and %d is %.2f\n", a, b, quotient);
-    } else {
-        printf("Error: Division by zero is undefined.\n");
-    }
+
+    c = a + b;
+
+    printf("The sum of %d and %d is %d\n", a, b, c);
+
     return 0;
 }
