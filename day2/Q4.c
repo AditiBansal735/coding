5341ada5935493e702aa45c6f39341a48452ffdb
Aditi Bansal <aditibansal7355@gmail.com>
2026-08-12 00:09:40 +0530

    commit

---
+++ day2/Q4.c
@@ -0,0 +1,21 @@
+/*
+ C program to calculate area and circumference of a circle
+ using the radius provided by the user.
+ */
+#include <stdio.h>
+
+int main()
+{
+    float r, area, circumference;
+
+    printf("Enter radius: ");
+    scanf("%f", &r);
+
+    area = 3.14 * r * r;
+    circumference = 2 * 3.14 * r;
+
+    printf("Area = %f\n", area);
+    printf("Circumference = %f", circumference);
+
+    return 0;
+}
