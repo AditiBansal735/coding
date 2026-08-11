1250b6e366da5bc301f9733d50402b0f59254054
Aditi Bansal <aditibansal7355@gmail.com>
2026-08-11 23:34:33 +0530

    commit

--- day1/Q2.c
+++ day1/Q2.c
@@ -2,25 +2,12 @@
 a program to input two numbers and display their sum, difference, product, and quotient.
 */
 #include <stdio.h>
-int main ()
+int main()
 {
-int a;
-int b;
-int c;
-int d;
-int e;
-int f;
-printf("enter the value of a");
-scanf("%d",&a);
-printf("enter value of b");
-scanf("%d",&b);
-c=a+b;
-printf("The sum of %d and %d is %d\n",a,b,c);
-d=a-b;
-printf("The difference of %d and %d is %d\n",a,b,d);
-e=a*b;
-printf("The product of %d and %d is %d\n",a,b,e);
-f=a/b;
-printf("The quotient of %d and %d is %d\n",a,b,f);
-return 0;
+    int a = 4, b = 2;
+    printf("Sum = %d\n", a + b);
+    printf("Difference = %d\n", a - b);
+    printf("Product = %d\n", a * b);
+    printf("Quotient = %d\n", a / b);
+    return 0;
 }
