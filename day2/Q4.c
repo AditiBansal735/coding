d935a48f3807826a9793a45dc98932c60c131880
Aditi Bansal <aditibansal7355@gmail.com>
2026-08-12 00:10:06 +0530

    commit

--- day2/Q4.c
+++ day2/Q4.c
@@ -3,19 +3,14 @@
  using the radius provided by the user.
  */
 #include <stdio.h>
-
 int main()
 {
     float r, area, circumference;
-
     printf("Enter radius: ");
     scanf("%f", &r);
-
     area = 3.14 * r * r;
     circumference = 2 * 3.14 * r;
-
     printf("Area = %f\n", area);
     printf("Circumference = %f", circumference);
-
     return 0;
 }
