d044d087886331cd638a973ee8e5150b72eabcec
Aditi Bansal <aditibansal7355@gmail.com>
2026-08-11 23:53:25 +0530

    commit

---
+++ day2/Q3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int length, breadth;
+    int area, perimeter;
+    scanf("%d %d", &length, &breadth);
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+    // Display result
+    printf("Area=%d, Perimeter=%d\n", area, perimeter);
+    return 0;
+}
