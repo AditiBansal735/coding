f0ce3ff053110f1094f30fe0c738827409b6f339
Aditi Bansal <aditibansal7355@gmail.com>
2026-08-11 23:27:59 +0530

    commit

--- day1/Q2.c
+++ day1/Q2.c
@@ -1,21 +1,26 @@
 /*
-a program to input two numbers and display their sum
+a program to input two numbers and display their sum, difference, product, and quotient.
 */
-
 #include <stdio.h>
-
-int main() {
-    int a, b, c;
-
-    printf("Enter the value of a: ");
-    scanf("%d", &a);
-
-    printf("Enter the value of b: ");
-    scanf("%d", &b);
-
-    c = a + b;
-
-    printf("The sum of %d and %d is %d\n", a, b, c);
-
-    return 0;
+int main ()
+{
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("enter value of b");
+scanf("%d",&b);
+c=a+b;
+printf("The sum of %d and %d is %d\n",a,b,c);
+d=a-b;
+printf("The difference of %d and %d is %d\n",a,b,d);
+e=a*b;
+printf("The product of %d and %d is %d\n",a,b,e);
+f=a/b;
+printf("The quotient of %d and %d is %d\n",a,b,f);
+return 0;
 }
