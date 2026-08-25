048c833fa0a7dd4122e981e637d7de87c1e45284
AditiBansal <aditibansal7355@gmail.com>
2026-08-26 00:45:43 +0530

    commit

--- day1/Q1.c
+++ day1/Q1.c
@@ -4,14 +4,17 @@ a program to input two numbers and display their sum
 #include <stdio.h>
 int main ()
 {
-int a;
-int b;
-int c;
-printf("enter the value of a:");
+    int a,b,c;
+
+    printf("enter a= ");
 scanf("%d",&a);
-printf("enter the value of b:");
-scanf("%d",&b);
-c=a+b;
-printf("The sum of %d and %d is %d",a,b,c);
-return 0;
+    printf("enter b= ");
+    scanf("%d",&b);
+
+    c=a+b;
+    printf("sum of %d and %d is %d",a,b,c);
+
+    getchar();
+    return 0;
+
 }
---
+++ day3/Q5.C
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Basic C code\n");
+    return 0;
+}
