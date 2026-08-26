046325225eff43b0c6cef5950855a5b12ea06cb1
AditiBansal <aditibansal7355@gmail.com>
2026-08-27 02:34:55 +0530

    commit

---
+++ day6/Q12.C
@@ -0,0 +1,24 @@
+/*
+a program to input an integer and check if it is positive, negative, or zero   
+*/
+#include <stdio.h>
+int main ()
+{
+
+    int a;
+
+    printf("enter the value of a: ");
+    scanf("%d",&a);
+
+    if (a>0)
+    printf("positive");
+
+    else if (a<0)
+    printf("negative");
+
+    else
+    printf("zero");
+
+    return 0;
+
+}
