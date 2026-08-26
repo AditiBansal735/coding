7c88855fe7999698c4942bf8941ee017cfdc4294
AditiBansal <aditibansal7355@gmail.com>
2026-08-26 15:08:29 +0530

    commit

--- day3/Q5.C
+++ day3/Q5.C
@@ -1,6 +1,18 @@
+/*
+a program to convert temperature from celcius to fahrenheit.
+*/
 #include <stdio.h>
+int main()
+{
+    int celcius, fahrenheit;
 
-int main() {
-    printf("Basic C code\n");
+    printf("enter temperature in celcius= ");
+    scanf("%d",&celcius);
+
+    fahrenheit = (celcius * 9/5) + 32;
+
+    printf("temperature in fahrenheit is %d",fahrenheit);
     return 0;
+    
+
 }
