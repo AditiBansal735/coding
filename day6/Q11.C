bd75e904c9e4f9b622b77730600b6617579e51c4
AditiBansal <aditibansal7355@gmail.com>
2026-08-26 18:44:59 +0530

    commit

---
+++ day6/Q11.C
@@ -0,0 +1,21 @@
+/*
+a program to input an integer and check whether it is even or odd using if-else
+*/  
+
+#include <stdio.h>
+int main ()
+{
+
+    int a;
+   
+    printf("enter the value of a= ");
+    scanf("%d",&a);
+    
+    if (a%2==0)
+     printf("%d is even",a); 
+     
+     else
+  printf("%d is odd",a);
+     
+return 0;
+}
