7db53f3e6a4f54606f89d03672290e05f49b8c90
AditiBansal <aditibansal7355@gmail.com>
2026-09-03 00:10:58 +0530

    commit

---
+++ day15/Q29.C
@@ -0,0 +1,19 @@
+/*
+a program to calculate the factorial of a number
+*/
+#include <stdio.h>
+int main()
+{
+int i, n, fact=1;
+
+printf("enter the value of n: ");
+scanf("%d", &n);
+
+for (i=1; i<=n; i++)
+{   
+    fact = fact * i;
+
+}   
+printf("factorial of %d is: %d", n, fact);
+return 0;
+}
