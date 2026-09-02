a8975464118375b77df490060dcd626b22379b8f
AditiBansal <aditibansal7355@gmail.com>
2026-09-03 00:04:19 +0530

    commit

---
+++ day14/Q28.C
@@ -0,0 +1,18 @@
+/*
+a program to print the product of even numbers from 1 to n
+*/
+#include <stdio.h>
+int main()
+{
+    int i,n,product=1;
+printf("enter the value of n= ");
+    scanf("%d",&n);
+    
+    for (i=2; i<=n; i=i+2)
+    {
+        product= product*i;
+    }
+    printf("product of first %d even numbers is %d",n,product);
+
+    return 0;
+}
