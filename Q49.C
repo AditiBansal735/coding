4fda04d7e4a8fc877a1f6cf244a7e77a613853f7
AditiBansal <aditibansal7355@gmail.com>
2026-09-08 07:07:35 +0530

    COMMIT

---
+++ Q49.C
@@ -0,0 +1,19 @@
+/*
+a program to print the following pattern
+*/
+#include <stdio.h>
+int main ()
+{
+    int i,j;
+    
+    for (i=5; i>0; i--)
+    {
+        for (j=i; j<=5; j++)
+       { printf("%d",j);}
+    
+    printf("\n");
+}
+
+return 0;
+}
+
